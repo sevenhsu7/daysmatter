2accc8d8bddcd6f0a828962f1fd07160448fd88b
sevenhsu7 <sevenxu777@163.com>
2025-01-20 01:39:36 +0800

    Disable ESLint during build

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/daysmatter/'
-    : '/'
+    : '/',
+  lintOnSave: false  // 添加这行来禁用构建时的 lint
 }
