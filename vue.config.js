77bd2af2d3c01cb9839690dbea38118d706fe0a1
Seven Hsu <xushengwen@echo.tech>
2025-01-20 01:03:38 +0800

    fix deploying

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/daysmatter/'
+    : '/'
+}
+
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false  // 暂时关闭 lint，以便我们可以先运行起来
