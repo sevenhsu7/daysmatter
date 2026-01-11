57422129251fa9448ff4fda9e439b529a5edbbe0
Seven Hsu <xushengwen@echo.tech>
2026-01-11 15:03:40 +0800

    fix: 修复自定义域名的 publicPath 配置

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/daysmatter/'
-    : '/',
+  publicPath: '/',  // 使用自定义域名时，应该使用根路径
   lintOnSave: false  // 添加这行来禁用构建时的 lint
 }
